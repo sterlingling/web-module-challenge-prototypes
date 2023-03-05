06b7d612989890049195a42d4c656c41aad55020
Sterling Sanders <sterlingsanders723@gmail.com>
2023-03-04 18:14:31 -0800

    completed tasks

--- index.js
+++ index.js
@@ -15,10 +15,23 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+this.name = name;
+this.age = age;
+this.stomach =[];
 }
-
+Person.prototype.eat = function(edible){
+if(this.stomach.length < 10){
+  return this.stomach.push(edible);
+}
+}
+Person.prototype.poop = function(){
+  return this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name} , ${this.age}`;
+}
+const mary = new Person('Mary',50);
 
 /*
   TASK 2
@@ -36,8 +49,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model,milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
 }
 
 
@@ -49,8 +68,13 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+Person.call(this,name,age);
+this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 
